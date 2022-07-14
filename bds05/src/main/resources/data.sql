eff7203f4e9a2049c5c3c8e320b1f390b70f14cc
Thiago José Oliveira <thiago02_08@hotmail.com>
2022-07-14 09:22:37 -0300

    Add files via upload

---
+++ bds05/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+--tb_user
+INSERT INTO tb_user (name, email, password) VALUES ('Bob Brown', 'bob@gmail.com',
+'$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Ana Green', 'ana@gmail.com',
+'$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+--tb_role
+INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+--tb_user_role
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
